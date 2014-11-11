e23c4c669331a00b4fceaef43a27e95041babbb2
Yerkebulan Tulibergenov <yerkebulan@gmail.com>
2014-11-10 17:12:12 -0800

    Started creating WpfClient.

---
+++ WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClientsButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked Clients button!");
+        }
+
+        private void StockButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked Stocks button!");
+        }
+    }
+}
